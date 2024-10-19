c510b8cfa41a391546c58c6a5685c10076f8c1e1
Julian S. <200318@studierende.htl-donaustadt.at>
2024-10-19 17:03:03 +0200

    style: remove animations for mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
-import { motion } from "framer-motion";
 import { Moon02Icon, Sun02Icon } from "hugeicons-react";
 import { Button } from "@/components/ui/button";
 
@@ -22,46 +21,14 @@ export function ModeToggle({ children }: { children?: React.ReactNode }) {
         onClick={() => {
           theme === "dark" ? setTheme("light") : setTheme("dark");
         }}
-        className="flex w-full items-center justify-start gap-2 px-3"
+        className="flex w-full items-center justify-start gap-2"
       >
         {theme === "light" && (
-          <motion.div
-            key={theme}
-            initial={{
-              x: 40,
-              opacity: 0,
-            }}
-            animate={{
-              x: 0,
-              opacity: 1,
-            }}
-            transition={{
-              duration: 0.3,
-              ease: "easeOut",
-            }}
-          >
-            <Sun02Icon className="h-4 w-4 flex-shrink-0 text-neutral-700 dark:text-neutral-500" />
-          </motion.div>
+          <Sun02Icon className="h-5 w-5 flex-shrink-0 text-muted-foreground" />
         )}
 
         {theme === "dark" && (
-          <motion.div
-            key={theme}
-            initial={{
-              x: 40,
-              opacity: 0,
-            }}
-            animate={{
-              x: 0,
-              opacity: 1,
-            }}
-            transition={{
-              ease: "easeOut",
-              duration: 0.3,
-            }}
-          >
-            <Moon02Icon className="h-4 w-4 flex-shrink-0 text-neutral-700 dark:text-neutral-500" />
-          </motion.div>
+          <Moon02Icon className="h-5 w-5 flex-shrink-0 text-muted-foreground" />
         )}
 
         <span className="sr-only">Toggle theme</span>
