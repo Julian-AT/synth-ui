54d8316be39e41b510a7b57338d55eadb0611830
Julian S. <200318@studierende.htl-donaustadt.at>
2024-10-19 17:01:20 +0200

    style: add custom styling to shadcn button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        "list-item":
+          "hover:bg-accent hover:text-accent-foreground flex w-full items-center justify-start gap-2 px-2",
       },
       size: {
         default: "h-9 px-4 py-2",
