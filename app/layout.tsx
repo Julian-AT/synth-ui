eb3818b0520f20207e7dd442e79829562d13ff5f
Julian S. <200318@studierende.htl-donaustadt.at>
2024-10-21 23:29:29 +0200

    chore: improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import { GeistSans } from "geist/font/sans";
-
 import "@/styles/globals.css";
-
 import { cn } from "@/lib/utils";
 import { Metadata, Viewport } from "next";
 import { Providers } from "@/components/providers";
@@ -9,22 +7,61 @@ import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const title = "Synth UI";
-const description = "Generative User Interfaces for the Web";
+const siteConfig = {
+  name: "Synth UI",
+  description: "Generative User Interfaces for the Web",
+  url: "https://synthui.design",
+  ogImage: "https://synthui.design/og.png",
+  creator: "Julian S.",
+  twitterHandle: "@julian-at",
+};
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://synthui.design"),
-  title,
-  description,
+  metadataBase: new URL(siteConfig.url),
+  title: siteConfig.name,
+  description: siteConfig.description,
   openGraph: {
-    title,
-    description,
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    siteName: siteConfig.name,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
   },
   twitter: {
-    title,
-    description,
     card: "summary_large_image",
-    creator: "@julian-at",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: siteConfig.twitterHandle,
+  },
+  keywords: [
+    "synth ui",
+    "UI design",
+    "UI generation",
+    "AI",
+    "generative UI",
+    "generative design",
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Shadcn UI",
+  ],
+  authors: [{ name: siteConfig.creator, url: "https://github.com/julian-at" }],
+  creator: siteConfig.creator,
+  manifest: `${siteConfig.url}/site.webmanifest`,
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
   },
 };
 
