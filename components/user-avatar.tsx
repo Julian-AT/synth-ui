958a7bf3c84adc85d44fbb7ce09341479de08589
Julian S. <200318@studierende.htl-donaustadt.at>
2024-10-21 09:11:24 +0200

    refactor: clean up user avatar

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -18,16 +18,13 @@ export default function UserAvatar({ className, ...props }: UserAvatarProps) {
 
   if (!user || !isMounted)
     return (
-      <div className="flex h-full w-full items-center justify-center rounded-sm border p-0">
+      <div className="flex h-full w-full items-center justify-center p-0">
         <Skeleton className="h-full w-full" />
       </div>
     );
 
   return (
-    <Avatar
-      className={cn("h-full w-full rounded-sm p-0", className)}
-      {...props}
-    >
+    <Avatar className={cn("h-full w-full p-0", className)} {...props}>
       <AvatarImage src={user.imageUrl} />
       <AvatarFallback className="flex items-center justify-center bg-background">
         <span className="sr-only">{user.emailAddresses[0].emailAddress}</span>
