91dcc130a2fef26cc2fff790501f0c2f1b44697a
Julian S. <200318@studierende.htl-donaustadt.at>
2024-10-23 23:37:30 +0200

    chore: remove relational database utils

--- lib/db/drizzle.ts
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import { config } from "dotenv";
-import * as schema from "@/lib/db/schema";
-
-config({ path: ".env.local" });
-
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
