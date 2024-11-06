b25eb3c92e2a9ab6407773d08ce42a8a237f33a9
Julian S. <200318@studierende.htl-donaustadt.at>
2024-11-07 00:35:56 +0100

    style: unify mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,10 +18,11 @@ export function ModeToggle({ children }: { children?: React.ReactNode }) {
     isClient && (
       <Button
         variant="ghost"
+        size="icon"
         onClick={() => {
           theme === "dark" ? setTheme("light") : setTheme("dark");
         }}
-        className="flex w-full items-center justify-start gap-2"
+        className="flex w-full items-center justify-start gap-2 px-4"
       >
         {theme === "light" && (
           <Sun02Icon className="h-5 w-5 flex-shrink-0 text-muted-foreground" />
